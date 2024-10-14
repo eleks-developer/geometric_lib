6981648bff9a66121d68ef38355dee78dfa23a3a
eleks-developer <[beloglazove.info@gmail.com]>
2024-10-14 13:10:32 +0300

    Added documentation for file functions circle.py , and also slightly changed the functions themselves

--- circle.py
+++ circle.py
@@ -2,9 +2,24 @@ import math
 
 
 def area(r: float) -> float:
-    return math.pi * r * r
+    '''
+    Вычисляет и возвращает площадь круга.
+            Параметры:
+                    r (float): радиус круга, вещественное число
+            Возвращаемое значение:
+                    c_area (float): площадь круга, вычесленная по формуле c_area = pi * r ^ 2
+    '''
+    c_area = math.pi * r * r
+    return c_area
 
 
 def perimeter(r: float) -> float:
-    return 2 * math.pi * r
-
+    '''
+    Вычисляет и возвращает периметр круга.
+            Параметры:
+                    r (float): радиус круга, вещественное число
+            Возвращаемое значение:
+                    c_perimeter (float): периметр круга, вычесленный по формуле c_perimeter = 2 * pi * r
+    '''
+    c_perimeter = 2 * math.pi * r
+    return c_perimeter
